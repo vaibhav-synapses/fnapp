c74aaaad6eb30a81fd7c2c77293dac20fafc728d
vaibhav <vaibhav.synapsesai@gmail.com>
2021-04-07 13:06:41 +0530

    2+2

--- fnapp-hello-world/index.js
+++ fnapp-hello-world/index.js
@@ -2,7 +2,7 @@ module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     
-    const responseMessage = 2+2
+    const responseMessage = "YOUR PLACEMENT ERA BEGINS"
     context.res = {
         // status: 200, /* Defaults to 200 */
         body: responseMessage
