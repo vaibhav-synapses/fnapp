555ead2928c243b0360af8896b7b544453661162
vaibhav <vaibhav.synapsesai@gmail.com>
2021-04-07 13:03:40 +0530

    2+2

--- fnapp-hello-world/index.js
+++ fnapp-hello-world/index.js
@@ -2,7 +2,7 @@ module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     
-    const responseMessage ="Hello new old  World change "
+    const responseMessage = 2+2
     context.res = {
         // status: 200, /* Defaults to 200 */
         body: responseMessage
