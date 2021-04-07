472e664cb702fe684d07470c629816b8b616fe9e
vaibhav <vaibhav.synapsesai@gmail.com>
2021-04-07 13:16:35 +0530

    edited

--- fnapp-hello-world/index.js
+++ fnapp-hello-world/index.js
@@ -2,7 +2,7 @@ module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     
-    const responseMessage = "YOUR PLACEMENT ERA BEGINS"
+    const responseMessage = "YOUR TIME BEGINS"
     context.res = {
         // status: 200, /* Defaults to 200 */
         body: responseMessage
