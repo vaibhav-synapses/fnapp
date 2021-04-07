bf377dac0b70b70ca80d383dc69d68e795803f86
vaibhav <vaibhav.synapsesai@gmail.com>
2021-04-07 12:58:55 +0530

    changed

--- fnapp-hello-world/index.js
+++ fnapp-hello-world/index.js
@@ -2,7 +2,7 @@ module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     
-    const responseMessage ="Hello new old  World "
+    const responseMessage ="Hello new old  World change "
     context.res = {
         // status: 200, /* Defaults to 200 */
         body: responseMessage
