ad7ab63f7804281b98cece810b0968de6927f281
vaibhav <vaibhav.synapsesai@gmail.com>
2021-04-07 17:51:02 +0530

    added new line

--- fnapp-hello-world/index.js
+++ fnapp-hello-world/index.js
@@ -2,7 +2,7 @@ module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     
-    const responseMessage = "YOUR TIME BEGINS"
+    const responseMessage = "corona cases increasing day by day"
     context.res = {
         // status: 200, /* Defaults to 200 */
         body: responseMessage
